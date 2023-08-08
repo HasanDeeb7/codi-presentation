e57357cbba0e66f482e2449dc0f5c7ee4286e4b2
Hasan Deeb <Sektories7@gmail.com>
2023-08-08 17:22:36 +0300

    added functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 
+const button = document.createElement("input")
+button.type = 'button'
+button.value = 'Close'
+button.id = 'close-btn'
+button.addEventListener('click', ()=>closeModal())
 function closeModal(){
         const modal = document.querySelector("#quit-smoking-modal");
         console.log(modal)
         modal.style.display = 'none'
     }
     function openModal(){
-         document.querySelector("#quit-smoking-modal").style = 'flex'
+         document.querySelector("#quit-smoking-modal").style.display = 'flex'
     }
     function submit(){
-     document.getElementById('form-container').innerHTML = 'Thank you for submitting'
+     const form = document.getElementById("form-container");
+     form.innerHTML = 'Your Response Have Been Submitted Successfully, Thank You! '
+     form.appendChild(button)
+
     }
